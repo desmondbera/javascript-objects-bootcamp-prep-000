7f988abbd96416da4323e605f42773ba9b94cc35
desmond beramendi <desmond7187@gmail.com>
2018-01-05 19:25:40 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = new Object(artistName: "songTitle");
+var playlist = new Object({keys: "songTitle"});
 
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign( {}, { [artistName]: songTitle})
